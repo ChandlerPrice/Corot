f52b028703470e1f1b3e5c2d9afa9fc4b5a87a58
Brandon Goldsworthy <BGoldsworthy@neumont.edu>
2020-11-10 10:13:10 -0700

    Added people abstract class

---
+++ Corot/People/People.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corot.People
+{
+    abstract class People
+    {
+        //Stats
+        int engineering;
+        int combat;
+        int medical;
+        int scavenging;
+        int leadership;
+
+        //Modifiers
+        bool alive;
+        bool injured;
+        int special;
+
+        //Equiptment
+        string weapon;
+        string tool;
+
+        /*
+         Potential
+        int happiness;
+         */
+    }
+}
