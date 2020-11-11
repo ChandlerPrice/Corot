106a1c252e07945e28caf2e89cbe0ee3f149b923
ChanSnake <fearthedarc@gmail.com>
2020-11-10 19:11:53 -0700

    Game class and Random Daily Events
    Game class containing food, population, and day
    Every day a Random Event is called

---
+++ Corot/Events/RandomEvents.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Corot.Events
+{
+    class RandomEvents
+    {
+        public enum randomDailyEventsEnum
+        {
+            zombieAttack,
+            zombieHordeAttack,
+            survivorDied,
+            survivorFound,
+            extraFood,
+            rottedFood,
+            boringDay
+        }
+
+        private static List<randomDailyEventsEnum> dailyEvents = new List<randomDailyEventsEnum>()
+        {
+            randomDailyEventsEnum.zombieAttack, randomDailyEventsEnum.zombieHordeAttack, randomDailyEventsEnum.survivorDied,
+            randomDailyEventsEnum.survivorFound, randomDailyEventsEnum.extraFood, randomDailyEventsEnum.rottedFood,
+            randomDailyEventsEnum.boringDay
+        };
+
+        public static randomDailyEventsEnum RandomDailyEvent()
+        {
+            Random random = new Random();
+            int r = random.Next(dailyEvents.Count);
+
+            return dailyEvents[r];
+        }
+
+
+    }
+}
---
+++ Corot/Game.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Corot.Events.RandomEvents;
+
+namespace Corot
+{
+    class Game
+    {
+        private Random rand = new Random();
+        private int Day;
+        private int Population;
+        private int Food;
+        public int day { get { return Day; } set { Day = value; } }
+        public int population { get { return Population; } set { Population = value; } }
+        public int food { get { return Food; } set { Food = value; } }
+
+        public void DailyEvent()
+        {
+            randomDailyEventsEnum dailyEvent = new randomDailyEventsEnum();
+            dailyEvent = RandomDailyEvent();
+            switch (dailyEvent)
+            {
+                case randomDailyEventsEnum.zombieAttack:
+                    break;
+                case randomDailyEventsEnum.zombieHordeAttack:
+                    break;
+                case randomDailyEventsEnum.survivorDied:
+                    Population -= rand.Next(3);
+                    break;
+                case randomDailyEventsEnum.survivorFound:
+                    Population += rand.Next(3);
+                    break;
+                case randomDailyEventsEnum.extraFood:
+                    Food += rand.Next(10);
+                    break;
+                case randomDailyEventsEnum.rottedFood:
+                    Food -= rand.Next(10);
+                    break;
+                case randomDailyEventsEnum.boringDay:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
